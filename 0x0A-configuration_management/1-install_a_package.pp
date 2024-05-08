0b2f540a13354bafbf820a1702674db5962e04ec
golden-licht <luminoonimul@gmail.com>
2024-05-08 16:41:56 +0300

    Add depdendency to flask installation

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# install flask
+# install flask and a dependency werkzeug
 package { 'flask':
   ensure   => '2.1.0',
   name     => 'flask',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure => '2.1.0',
+  name => 'werkzeug',
   provider => 'pip3',
 }
