d369385de745496ea05f62a4bdf48418605597bd
golden-licht <120052668+golden-licht@users.noreply.github.com>
2024-05-08 17:19:56 +0300

    remove werkzeug package from list of packages to be installed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
-# install flask and a dependency werkzeug
+# install flask
 package { 'flask':
   ensure   => '2.1.0',
   name     => 'flask',
-  provider => 'pip3',
-}
-
-package { 'werkzeug':
-  ensure   => '2.1.1',
-  name     => 'werkzeug',
   provider => 'pip3',
 }
