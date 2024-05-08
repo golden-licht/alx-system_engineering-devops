77acb4aa135b3ed1d70f14e733bb6199190cbbb1
golden-licht <120052668+golden-licht@users.noreply.github.com>
2024-05-08 17:26:36 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,10 @@
-# install flask
+# install flask and werkzeug
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  name     => 'werkzeug',
+  provider => 'pip3',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   name     => 'flask',
