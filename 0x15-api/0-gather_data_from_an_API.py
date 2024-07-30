c273db57b2cf07f39e4b7f073b87fc17b39af78d
golden-licht <luminoonimul@gmail.com>
2024-07-30 09:28:16 +0300

    Print info about progress of an employee fetched from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+For a given employee ID, passed as arguement, prints information
+about his/her TODO list progress, fetching from an API
+"""
+
+import json
+import sys
+import urllib.request as request
+
+if __name__ == '__main__':
+    # users/employees
+    with request.urlopen('https://jsonplaceholder.typicode.com/users') as response:
+        users = json.loads(response.read())
+
+    # task todos
+    with request.urlopen('http://jsonplaceholder.typicode.com/todos') as response:
+        todos = json.loads(response.read())
+
+    # the id of the employee we eant info for
+    employee_id = int(sys.argv[1])
+
+    # get the employee name
+    for user in users:
+        if user.get('id') == employee_id:
+            employee_name = user.get('name')
+            break
+
+    # get the tasks done by certain employee
+    total_taks = 0
+    tasks_done = []
+    for todo in todos:
+        if todo.get('userId') == employee_id:
+            total_taks += 1
+            if todo.get('completed'):
+                tasks_done.append(todo.get('title'))
+
+    # print the info
+    print(
+        f'Employee {employee_name} is done with tasks'
+        f'({len(tasks_done)}/{total_taks}):'
+    )
+    for task in tasks_done:
+        print(f'\t{task}')
