03d6c1adf069126f1cc14118d7c37644a095431d
golden-licht <luminoonimul@gmail.com>
2024-08-17 09:53:41 +0300

    Query the reddit api and return the numbe rof subs for a given subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Contains a function that queries the reddit api and returns
+the number of subscribers for a particular subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f'https://www.reddit.com/r/{subreddit}/about/.json'
+    headers = {'user-agent': 'my-app/0.0.1'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    else:
+        return 0
