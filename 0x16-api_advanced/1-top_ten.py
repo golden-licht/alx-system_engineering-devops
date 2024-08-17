6184e5be8700494071651f2ac68d14bc99d097ab
golden-licht <luminoonimul@gmail.com>
2024-08-17 13:17:02 +0300

    Query the reddit api and print the top 10 hot posts for a particular subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Contains a function that queries the reddit api and prints
+the titles of the first 10 hot posts listed for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    url = f'https://www.reddit.com/r/{subreddit}/top/.json?count=0&limit=10'
+    headers = {'user-agent': 'my-app/0.0.1'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        children = response.json()['data']['children']
+        for child in children:
+            print(child['data']['title'])
+    else:
+        print(None)
