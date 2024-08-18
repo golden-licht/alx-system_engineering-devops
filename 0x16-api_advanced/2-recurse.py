31468f5c5b75d9a96455c1ac9e15796e9c63e7b5
golden-licht <luminoonimul@gmail.com>
2024-08-18 11:28:12 +0300

    Update /top with /hot

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,10 +8,15 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after='', count=0):
-    url = f'''https://www.reddit.com/r/
-    {subreddit}/hot/.json?after={after}&count={count}'''
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
     headers = {'user-agent': 'my-app/0.0.1'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    payload = {'after': after, 'count': count}
+    response = requests.get(
+        url,
+        headers=headers,
+        params=payload,
+        allow_redirects=False
+    )
 
     if response.status_code != 200:
         return None
