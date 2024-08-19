0ae181718ecc81b7a0e62d5e320ed9e56231a703
golden-licht <luminoonimul@gmail.com>
2024-08-19 07:52:58 +0300

    Simpler command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
 #Replace the require statement for '/class-wp-locale.phpp' with '/class-wp-locale.php' in wp-settings.php
 exec { 'fix_require_statement':
-  command => '/bin/sed -i "s#require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );' \
-  '#require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );#" /var/www/html/wp-settings.php',
+  command => /bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php
 }
