5c6af7bf4e4bf6f3b40e87ef1dcd1638cb91c12c
golden-licht <luminoonimul@gmail.com>
2024-08-19 07:55:33 +0300

    Simpler command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 #Replace the require statement for '/class-wp-locale.phpp' with '/class-wp-locale.php' in wp-settings.php
 exec { 'fix_require_statement':
-  command => /bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php
+  command => "/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"
 }
